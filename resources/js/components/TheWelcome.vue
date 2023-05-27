4f3e4aadc6896e24bb75f9dab7f0b6a34bfe18d5
merpich <merpich.dev@gmail.com>
2023-05-27 21:44:16 +0300

    let's begin

---
+++ resources/js/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="max-w-sm mx-auto py-[20vh]">
+        <h1 class="mb-14 text-4xl text-center">Blank project</h1>
+        <ul class="flex justify-center gap-6 mx-auto">
+            <li class="text-xl">Laravel</li>
+            <li class="text-xl">+</li>
+            <li class="text-xl">Vue</li>
+            <li class="text-xl">+</li>
+            <li class="text-xl">Inertia</li>
+        </ul>
+    </div>
+</template>
